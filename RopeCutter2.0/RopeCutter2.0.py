47126ed4a1b247f1d9d38f3c682b13b8d5b7b4ed
Pasha4oo <minecraftdog@tutanota.com>
2024-04-19 22:43:12 +0300

    delete some stuff

--- RopeCutter2.0/RopeCutter2.0.py
+++ RopeCutter2.0/RopeCutter2.0.py
@@ -2,13 +2,9 @@ from itertools import product
 
 n = []
 m = []
-m1 = []
-m2 = []
-m3 = []
 break_ = 0
 times = 6
-many_dict = []
-number = 14
+number = 20
 dict1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 def add(place):
     if place not in n and place > 0 and place <= number:
@@ -29,10 +25,6 @@ def add2(place1, place2):
         print(n)
 for j in range(1, number + 1):
     m.append(j)
-    m1.append(j)
-    m2.append(j)
-    m3.append(j)
-    many_dict.append([o for o in range(1, number + 1)])
 for d1 in m:
     if break_ == 1:
         break
